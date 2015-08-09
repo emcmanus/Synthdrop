a973e58da47699cb36f02e91738f7ac815477e56
Ed McManus <emcmanus@gmail.com>
2015-08-08 17:20:58 -0700

    Add Bootstrap skinned views for Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  layout :layout_by_resource
+
+  protected
+
+    def layout_by_resource
+      devise_controller? ? "devise" : "application"
+    end
 end
