0de3335713547eafe2be8d90d617c7e9e0ddb482
Ed McManus <emcmanus@gmail.com>
2015-08-08 19:39:31 -0700

    Configure paperclip to use S3.

--- README.markdown
+++ README.markdown
@@ -1,1 +1,10 @@
+## Synthdrop
 
+#### General config
+
+1. Copy .env to .env.example and add values
+
+#### S3 Configuration
+
+1. Set environment variables
+1. Enable versioning on production bucket
--- app/models/script.rb
+++ app/models/script.rb
@@ -1,3 +1,8 @@
 class Script < ActiveRecord::Base
   belongs_to :user
+  has_attached_file :content, storage: :s3, s3_credentials: {
+    access_key_id: ENV['S3_ACCESS_KEY_ID'],
+    secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+    bucket: ENV['S3_BUCKET_NAME'],
+  }
 end
