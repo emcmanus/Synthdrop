1432a6af17d92f5b33eb19a25268d25d148d57f0
Ed McManus <emcmanus@gmail.com>
2015-08-23 01:51:56 -0700

    Add default content for new scripts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def is_editor?
     controller.controller_name == 'scripts' && controller.action_name == 'editor'
   end
+
+  def script_content_url(script)
+    if script.has_content?
+      script.content_url
+    else
+      'https://s3-us-west-1.amazonaws.com/synthdrop/scripts/dc8606ea6d98885e517e898cb60ca304'
+    end
+  end
 end
